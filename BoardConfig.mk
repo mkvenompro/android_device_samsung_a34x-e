a417cbb1315196bb7012e09204e3fc2648747473
Jamie Falconer <mrfluffyoven@gmail.com>
2026-04-02 16:25:17 +0100

    add Samsung and mediatek compatibility matrix

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -101,6 +101,9 @@ BOARD_AVB_VENDOR_BOOT_ROLLBACK_INDEX_LOCATION := 1
 
 # VINTF
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
+    hardware/samsung/vintf/samsung_framework_compatibility_matrix.xml \
+    hardware/mediatek/vintf/mediatek_framework_compatibility_matrix.xml
 
 # Inherit the proprietary files
 include vendor/samsung/a34x/BoardConfigVendor.mk

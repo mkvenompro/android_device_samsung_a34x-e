a8a123ffbf88a17d66ed8315191411c54d10daa3
Jamie Falconer <mrfluffyoven@gmail.com>
2026-04-04 12:03:03 +0100

    Update fstab file references in device.mk

--- device.mk
+++ device.mk
@@ -81,7 +81,7 @@ PRODUCT_PACKAGES += \
     init.recovery.samsung.rc \
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/rootdir/etc/fstab.ramplus:$(TARGET_COPY_OUT_RAMDISK)/fstab.ramplus
+    $(LOCAL_PATH)/rootdir/etc/fstab.mt6877:$(TARGET_COPY_OUT_RAMDISK)/fstab.mt6877
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \

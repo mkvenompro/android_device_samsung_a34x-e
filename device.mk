5ff4b1dcfc92cf865c2b5585ef52db019c2116ef
Jamie Falconer <mrfluffyoven@gmail.com>
2026-04-04 15:56:49 +0100

    add missing seperator

--- device.mk
+++ device.mk
@@ -22,7 +22,7 @@ PRODUCT_PACKAGES += \
     fastbootd
 
 PRODUCT_PACKAGES += \
-    android.hardware.gatekeeper@1.0-service
+    android.hardware.gatekeeper@1.0-service \
     android.hardware.gatekeeper@1.0-impl
 
 # Health

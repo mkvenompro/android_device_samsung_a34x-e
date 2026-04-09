9c15a11122bc9bc42fa1d0a0d567c4561131f84e
Jamie Falconer <mrfluffyoven@gmail.com>
2026-04-09 07:22:26 +0100

    cleanup sepolicy

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -160,10 +160,10 @@ BOARD_AVB_VBMETA_SYSTEM_KEY_PATH := external/avb/test/data/testkey_rsa4096.pem
 BOARD_AVB_VBMETA_SYSTEM_ROLLBACK_INDEX := $(PLATFORM_SECURITY_PATCH_TIMESTAMP)
 BOARD_AVB_VBMETA_SYSTEM_ROLLBACK_INDEX_LOCATION := 2
 
-BOARD_SEPOLICY_DIRS += \
-    hardware/mediatek/sepolicy/basic \
-    hardware/mediatek/sepolicy/dynamic \
-    hardware/samsung/sepolicy
+# Sepolicy
+include device/mediatek/sepolicy_vndr/SEPolicy.mk
+include device/lineage/sepolicy/libperfmgr/sepolicy.mk
+
 
 # VINTF
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml

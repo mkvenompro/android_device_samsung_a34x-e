c51a65abb0198b3854dfaf6d4083333e5bf1a93e
Jamie Falconer <mrfluffyoven@gmail.com>
2026-04-10 09:15:31 +0100

    Delete android_filesystem_config.h

--- android_filesystem_config.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <private/android_filesystem_config.h>
-
-#define AID_VENDOR_SPAY 2906  // confirm actual UID from stock /vendor/etc/passwd
-
-static const struct android_id_info vendor_android_ids[] = {
-    {"vendor_spay", AID_VENDOR_SPAY},
-};

3ed8ede890141fe4930ccce0c661ef8df57f96d0
Jamie Falconer <mrfluffyoven@gmail.com>
2026-04-10 10:04:40 +0100

    Add noticefile

--- device.mk
+++ device.mk
@@ -80,6 +80,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/fstab.mt6877:$(TARGET_COPY_OUT_RAMDISK)/fstab.mt6877 \
     $(LOCAL_PATH)/rootdir/etc/fstab.mt6877:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.mt6877
 
+LOCAL_NOTICE_FILE := $(LOCAL_PATH)/NOTICE.xml.gz
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)

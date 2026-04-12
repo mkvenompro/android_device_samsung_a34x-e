ed87cc0d0df429abbcc02a6df5760ae825ff741e
SavedByLight <mrfluffyoven@gmail.com>
2026-04-12 13:50:33 +0100

    change libnfc-nci.conf to system

--- device.mk
+++ device.mk
@@ -304,7 +304,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/nfc/libnfc-sec-vendor.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-sec-vendor.conf
-        $(LOCAL_PATH)/configs/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nci.conf
+    $(LOCAL_PATH)/configs/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_SYSTEM)/etc/libnfc-nci.conf
 
 # RIL
 PRODUCT_COPY_FILES += \

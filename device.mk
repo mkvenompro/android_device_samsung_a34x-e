bcc0166241bb2b10c3d6e02ac1b3daa9a6c8ee43
Jamie Falconer <mrfluffyoven@gmail.com>
2026-04-19 19:59:10 +0100

    Disable PRODUCT_ENFORCE_VINTF_MANIFEST

--- device.mk
+++ device.mk
@@ -3,6 +3,7 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
+PRODUCT_ENFORCE_VINTF_MANIFEST := false
 
 # Enable project quotas and casefolding for emulated storage without sdcardfs
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)

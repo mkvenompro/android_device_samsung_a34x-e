4e46766f99a1fa3e7295aac94fd2f5ff5c8805d7
MkVenomPro <mkvenompro@users.noreply.github.com>
2026-08-23 10:11:55 -0700

    fix: restore DTB bootimg, fix XML, add BT header, enable sysdlkm image
    - Restore BOARD_INCLUDE_DTB_IN_BOOTIMG := true (recovery.img needs dtb.img)
    - Fix stray 'l' character in device manifest.xml (invalid XML)
    - Create bluetooth/include/bt_vendor_config.h (missing BT HAL dep)
    - Set BOARD_BUILD_SYSTEM_DLKM_IMAGE := true (was false but partition is enabled)

---
+++ bluetooth/include/bt_vendor_config.h
@@ -0,0 +1,11 @@
+/*
+ * Copyright (C) 2024 The LineageOS Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#pragma once
+
+#define BT_NV_SUPPORTS_SSP true
+#define BT_NV_SUPPORTS_SECURE_CONNECTIONS true
+#define BT_NV_SUPPORTS_DSB true
